5f43feb80bcefc4ae0819d8e1707b4d2c5e18e36
= <=>
2024-07-31 21:17:05 +0545

    10EvenNumber

---
+++ Question3/EvenNumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    int count = 0;
+
+    System.out.println("First 10 even numbers are - ")
+
+    for(int i=0;count!=10;i++){
+        if(i%2==0){
+            System.out.println(i+" ");
+            count++;
+        }
+    }
+}
